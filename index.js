962c287eadf0c1c1cbcc991211a18d87002afce2
Mkuu205 <Ndegwabrian022@gmail.com>
2026-02-23 07:50:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-__path = process.cwd()
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 8000;
-let server = require('./qr'),
-code = require('./pair');
+
+let server = require('./qr');
+let code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
+
+/* ---------- middleware FIRST ---------- */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* ---------- routes ---------- */
 app.use('/qr', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
+
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
 
 app.get('/fork-check', (req, res) => {
   res.sendFile(path.join(__dirname, 'fork-check.html'));
 });
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(port, () => {
-    console.log(`📡 Connected on http://localhost:` + port)
-})
 
-module.exports = app
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+/* ---------- start ---------- */
+app.listen(port, () => {
+  console.log(`📡 Connected on http://localhost:${port}`);
+});
+
+module.exports = app;
