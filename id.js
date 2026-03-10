9867e75867591c4935b0f547576e35fc260df3e6
Mkuu205 <Ndegwabrian022@gmail.com>
2026-03-10 20:13:53 +0300

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,12 @@
-function makeid(num = 4) {
+function makeid(length = 4) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+
+  for (let i = 0; i < length; i++) {
+    result += chars[Math.floor(Math.random() * chars.length)];
   }
+
   return result;
 }
-module.exports = {makeid};
+
+module.exports = { makeid };
