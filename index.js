c84d814a871631d16be407479eb74c7aca73e89b
Mkuu205 <Ndegwabrian022@gmail.com>
2026-03-11 04:06:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ const qrRoutes = require('./qr');
 const codeRoutes = require('./pair');
 
 
-/*----'----------------------*/
-const data = fs.readFileSync(file)
-fs.unlinkSync(file) // delete after download
-res.json(JSON.parse(data))
-
 /* -------------------- FIX EVENT LIMIT -------------------- */
 
 require('events').EventEmitter.defaultMaxListeners = 500;
